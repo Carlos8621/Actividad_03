32f89305aa9ad052da7508273b815f7765458d07
Carlos Ordaz <109123775+Carlos8621@users.noreply.github.com>
2022-07-21 12:26:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+Nombre_usuario_array=[ ]
+function submit(){
+    var Nombre_1 = document.getElementById("Nombre_Usuario_1").value
+    var Nombre_1 = document.getElementById("Nombre_Usuario_2").value
+    var Nombre_1 = document.getElementById("Nombre_Usuario_3").value
+    var Nombre_1 = document.getElementById("Nombre_Usuario_4").value
+
+    Nombre_usuario_array.push("Nombre_1");
+    Nombre_usuario_array.push("Nombre_2");
+    Nombre_usuario_array.push("Nombre_3");
+    Nombre_usuario_array.push("Nombre_4");
+}
